cc15a4b8f10cfa582c0ea58723983c7c40f8efe6
Mex Delgado <mdelgado@tresdoce.com.ar>
2022-03-29 20:39:20 -0300

    chore(demopkg): remove logs

--- packages/demopkg/src/greeting/services/greeting.service.ts
+++ packages/demopkg/src/greeting/services/greeting.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GreetingService {
   getHello(): string {
-    console.log('msg');
     return '¡Hello from the new package demopkg!';
   }
 }
