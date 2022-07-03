a8107e6bfb0646948291952d6c6d23a25ec03816
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2022-07-02 21:09:42 -0300

    chore(mexttest): remove console log

--- packages/mextest/src/greeting/services/greeting.service.ts
+++ packages/mextest/src/greeting/services/greeting.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GreetingService {
   getHello(): string {
-    console.log("this is a log")
     return '¡Hello from the new package mextest!';
   }
 }
