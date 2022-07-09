13c2db22ebd02a523f6f85182a41b2600f64830e
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2022-07-08 22:50:40 -0300

    chore(custompkg): add log in pkg

--- packages/custompkg/src/greeting/services/greeting.service.ts
+++ packages/custompkg/src/greeting/services/greeting.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GreetingService {
   getHello(): string {
+    console.log('custom pkg return msg');
     return '¡Hello from the new package custompkg!';
   }
 }
