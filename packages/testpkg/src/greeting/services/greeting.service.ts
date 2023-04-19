7f869c885e249a0181906ffcdcb7444732214dbf
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-04-19 12:24:12 -0300

    fix: some commit

--- packages/testpkg/src/greeting/services/greeting.service.ts
+++ packages/testpkg/src/greeting/services/greeting.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GreetingService {
   getHello(): string {
+    //some comment
     return '¡Hello from the new package testpkg!';
   }
 }
