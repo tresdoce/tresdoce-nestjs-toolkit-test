0b16e6e1d5574911a4287acb0c93c29e8ba365ea
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-04-18 23:25:42 -0300

    feat(mextest): change message

--- packages/mextest/src/greeting/services/greeting.service.ts
+++ packages/mextest/src/greeting/services/greeting.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GreetingService {
   sayHello(): string {
-    //change msg
     return '¡Hello from the new package mex test!';
   }
 }
