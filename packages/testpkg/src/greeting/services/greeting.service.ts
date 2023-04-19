51c944146c66dbb789ef28cd97e9e7e34c17370d
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-04-19 12:51:31 -0300

    fix(testpkg): remove comment

--- packages/testpkg/src/greeting/services/greeting.service.ts
+++ packages/testpkg/src/greeting/services/greeting.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GreetingService {
   getHello(): string {
-    // add comment
     return '¡Hello from the new package testpkg!';
   }
 }
