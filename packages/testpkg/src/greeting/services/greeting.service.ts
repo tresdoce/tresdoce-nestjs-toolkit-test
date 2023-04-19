5ade1c5cf9daf8044bcdbbc6394daed7386a9428
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-04-19 11:59:59 -0300

    chore(testpkg): edit service

--- packages/testpkg/src/greeting/services/greeting.service.ts
+++ packages/testpkg/src/greeting/services/greeting.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class GreetingService {
+  // sras
   getHello(): string {
     return '¡Hello from the new package testpkg!';
   }
