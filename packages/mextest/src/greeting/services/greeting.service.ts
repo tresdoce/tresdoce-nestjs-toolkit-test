59c7d252863e1bfeff9ea1062ae7521389464325
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-04-19 16:58:17 -0300

    chore(mextest): add comment

--- packages/mextest/src/greeting/services/greeting.service.ts
+++ packages/mextest/src/greeting/services/greeting.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GreetingService {
   sayHello(): string {
+    //sadsad
     return '¡Hello from the new package mex test!';
   }
 }
